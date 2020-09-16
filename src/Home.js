433fe72fd1cce276893eb6ab01b6d7bf65eb947d
Michael Silas <32719824+silasmichael@users.noreply.github.com>
2020-09-16 16:14:27 +0300

    update

--- src/Home.js
+++ src/Home.js
@@ -42,7 +42,7 @@ function Home() {
                         <div className="home-memu-category">
                             <span className="subtitle">Select Your Meal 🍗</span>
                             <h3 className="home-menu-category-h3">Popular  
-                            <strong className="home-menu-category-h3-strong">Foods</strong></h3>
+                            <strong className="home-menu-category-h3-strong"> Foods</strong></h3>
                             <ul>
                                 <li>
                                     <Link className="link">
@@ -79,6 +79,41 @@ function Home() {
                         </div>
                     </div>
                     <div className="home-dessert">
+                        <div className="home-dessert-title">
+                            <span className="subtitle">Choose Dessert 🍨</span>
+                            <h3 className="home-menu-category-h3">Popular  
+                            <strong className="home-menu-category-h3-strong"> Dessert</strong></h3>
+                        </div>
+                        <div className="home-dessert-container">
+                            <div className="home-dessert-container-item">
+                                <img src="https://untree.co/demos/meal/images/1x/dessert_1.png" alt= "Cherry  Muffin"/>
+                                <div className="item-detail">
+                                    <h3 id="title">Cherry  Muffin</h3>
+                                    <span id="price">15$</span>
+                                </div>
+                            </div>
+                            <div className="home-dessert-container-item">
+                                <img src="https://untree.co/demos/meal/images/1x/dessert_2.png " alt= "Rose Muffin"/>
+                                <div className="item-detail">
+                                    <h3 id="title">Rose Muffin</h3>
+                                    <span id="price">20$</span>
+                                </div>
+                            </div>
+                            <div className="home-dessert-container-item">
+                                <img src="https://untree.co/demos/meal/images/1x/dessert_3.png" alt= "Sweet Donut"/>
+                                <div className="item-detail">
+                                    <h3 id="title">Sweet Donut</h3>
+                                    <span id="price">18$</span>
+                                </div>
+                            </div>
+                            <div className="home-dessert-container-item">
+                                <img src="https://untree.co/demos/meal/images/1x/dessert_4.png" alt= "Choco Cake"/>
+                                <div className="item-detail">
+                                    <h3 id="title">Choco Cake</h3>
+                                    <span id="price">15$</span>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
 
